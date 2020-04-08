fbc2781cae02d875b15aae3950450c7d6ae43a1a
Ayrton <a.munoz3327@gmail.com>
2020-04-07 23:19:55 -0400

    added window and resolution to command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 use std::env;
+use std::convert::TryInto;
 use interpreter::Interpreter;
+
+extern crate sdl2;
+extern crate gl;
 
 mod common;
 mod interpreter;
@@ -25,15 +29,20 @@ fn check_flag(args: &Vec<String>, flags: &[&str]) -> bool {
       .any(|s| flags.iter().any(|t| *t == *s))
 }
 
+const DEFAULT_X: u32 = 320;
+const DEFAULT_Y: u32 = 240;
+const DEFAULT_RESOLUTION: [u32; 2] = [DEFAULT_X, DEFAULT_Y];
 const HELP_FLAGS: [&str;2] = ["-h", "--help"];
 const BIOS_FLAGS: [&str;2] = ["-b", "--bios"];
 const INFILE_FLAGS: [&str;2] = ["-i", "--input"];
 const STEPS_FLAGS: [&str;2] = ["-n", "--steps"];
 const LOG_FLAGS: [&str;2] = ["-l", "--log"];
-const ALL_FLAGS: [([&str;2],Option<&str>);5] = [(HELP_FLAGS, None),
+const RESOLUTION_FLAGS: [&str;2] = ["-s", "--size"];
+const ALL_FLAGS: [([&str;2],Option<&str>);6] = [(HELP_FLAGS, None),
                                                 (BIOS_FLAGS, Some("BIOS")),
                                                 (INFILE_FLAGS, Some("INFILE")),
                                                 (LOG_FLAGS, None),
+                                                (RESOLUTION_FLAGS, Some("WIDTHxHEIGHT")),
                                                 (STEPS_FLAGS, Some("n"))];
 
 fn print_help() {
@@ -54,14 +63,45 @@ fn main() -> io::Result<()> {
   let bios = get_arg(&args, &BIOS_FLAGS);
   let infile = get_arg(&args, &INFILE_FLAGS);
   let help = check_flag(&args, &HELP_FLAGS);
-  let steps = get_arg(&args, &STEPS_FLAGS).map(|steps| steps.parse::<u32>().unwrap());
+  let steps = get_arg(&args, &STEPS_FLAGS).map(|steps| steps.parse::<u32>().ok())
+                                          .flatten();
   let logging = check_flag(&args, &LOG_FLAGS);
+  let [wx, wy] = get_arg(&args, &RESOLUTION_FLAGS).map_or(DEFAULT_RESOLUTION,
+    |resolution| {
+      (*resolution.split("x")
+                  .take(2)
+                  .enumerate()
+                  .map(|(i, x)|
+                         x.parse::<u32>()
+                          .unwrap_or(DEFAULT_RESOLUTION[i]))
+                  .collect::<Vec<u32>>()
+                  .into_boxed_slice()).try_into().unwrap_or(DEFAULT_RESOLUTION)
+    }
+  );
 
   if help {
     print_help();
   } else {
     match bios {
       Some(bios_filename) => {
+        let sdl = sdl2::init().unwrap();
+        let video_subsystem = sdl.video().unwrap();
+        let window = video_subsystem.window("RSX", wx, wy)
+                                    .opengl()
+                                    .resizable()
+                                    .build()
+                                    .unwrap();
+        let event_pump = sdl.event_pump().unwrap();
+        let gl_context = window.gl_create_context().unwrap();
+        let gl = gl::load_with(
+          |s| {
+            video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
+          });
+        unsafe {
+          gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+          gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.gl_swap_window();
         Interpreter::new(bios_filename, infile)?.run(steps, logging);
       },
       None => {
