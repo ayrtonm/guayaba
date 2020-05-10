ab6c9fd38e33f7014d70233a5d77954201f22c3d
Ayrton <a.munoz3327@gmail.com>
2020-05-10 12:28:18 -0400

    added new function to toy JIT compiler

--- jit.rs
+++ jit.rs
@@ -6,9 +6,12 @@ fn main() {
     Box::new(mult_two)
   ];
   let source = vec![1, 2, 2, 1];
-  let program = source.iter().map(|&n| &functions[n]);
+  let mut program: Vec<&Box<dyn Fn(u32) -> u32>> =
+    source.iter().map(|&n| &functions[n]).collect();
+  let Fx = add_num(5);
+  program.push(&Fx);
   let input = 5;
-  let output = program.fold(input, |x, f| f(x));
+  let output = program.iter().fold(input, |x, f| f(x));
 
   let alt_func: Vec<Box<dyn Fn()>> = vec![
     Box::new(do_nothing_0),
@@ -16,7 +19,7 @@ fn main() {
   ];
   let alt_source = vec![0,1,0];
   let alt_program = alt_source.iter().map(|&n| &alt_func[n]);
-  let alt_output = alt_program.fold((), |_, f| f());
+  let alt_output = alt_program.for_each(|f| f());
   println!("main {}", output);
 }
 
@@ -24,13 +27,19 @@ fn identity(x: u32) -> u32 {
   println!("called identity");
   x
 }
+
+fn add_num(num: u32) -> Box<dyn Fn(u32) -> u32> {
+  Box::new(move |x| x + num)
+}
 fn add_three(x: u32) -> u32 {
   println!("called add_three");
+  println!("{} + 3 = {}", x, x + 3);
   x + 3
 }
 
 fn mult_two(x: u32) -> u32 {
   println!("called mult_two");
+  println!("{} * 2 = {}", x, x * 2);
   x * 2
 }
 
