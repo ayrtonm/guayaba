36b91e793fcd3157100997b84962787cb69c0a3a
Ayrton <a.munoz3327@gmail.com>
2020-05-17 02:46:48 -0400

    started working on the x64 JIT

---
+++ src/x64_jit/mod.rs
@@ -0,0 +1,185 @@
+use std::io;
+use std::collections::HashMap;
+use std::time::Instant;
+use std::time::Duration;
+use crate::console::Console;
+use crate::register::Register;
+
+mod insn_ir;
+mod opcodes;
+mod jumps;
+mod optimize;
+
+struct Stub {
+  operations: fn(),
+  final_pc: Register,
+  len: u32,
+}
+
+impl Stub {
+  fn execute(&self) {
+    (self.operations)()
+  }
+  fn final_pc(&self) -> Register {
+    self.final_pc
+  }
+  fn len(&self) -> u32{
+    self.len
+  }
+}
+
+pub struct X64JIT {
+  console: Console,
+  //maps start addresses to stubs for efficient execution
+  stubs: HashMap<Register, Stub>,
+  //maps end addresses to start addresses for efficient cache invalidation
+  ranges_compiled: HashMap<Register, Vec<Register>>,
+}
+
+impl X64JIT {
+  pub fn run(&mut self, n: Option<u32>, optimize: bool, logging: bool) {
+    let start_time = Instant::now();
+    let mut compile_time = start_time - start_time;
+    let mut cache_time = start_time - start_time;
+    let mut refresh_timer: i64 = Console::REFRESH_RATE;
+    println!("running in dummy JIT mode");
+    loop {
+      let address = Console::physical(self.console.r3000.pc());
+      let maybe_stub = self.stubs.get(&address);
+      match maybe_stub {
+        Some(stub) => {
+          *self.console.r3000.pc_mut() = stub.final_pc();
+          stub.execute();
+          //for f in operations {
+          //  self.console.r3000.flush_write_cache(&mut self.console.delayed_writes,
+          //                                     &mut self.console.modified_register);
+          //  f(&mut self.console);
+          //  match self.console.gpu.exec_next_gp0_command() {
+          //    Some(object) => self.console.screen.draw(object),
+          //    None => (),
+          //  };
+          //  self.console.cd.exec_command();
+          //}
+          refresh_timer -= stub.len() as i64;
+          if refresh_timer < 0 {
+            self.console.screen.refresh_window();
+            refresh_timer = Console::REFRESH_RATE;
+          }
+          self.console.i += stub.len();
+          n.map(|n| {
+            if self.console.i >= n {
+              let end_time = Instant::now();
+              panic!("Executed {} steps in {:?}\nwith {:?} of compile time and {:?} of cache time",
+                     self.console.i, end_time - start_time, compile_time, cache_time);
+            };
+          });
+          if self.console.overwritten.iter().any(|&t| address <= t && t <= stub.final_pc()) {
+            self.cache_invalidation(address);
+          }
+          self.console.overwritten.clear();
+          *self.console
+               .r3000
+               .pc_mut() = self.console
+                               .next_pc
+                               .map_or_else(|| self.console.r3000.pc().wrapping_add(4),
+                                            |next_pc| next_pc);
+          if !self.console.handle_events() {
+            return
+          }
+        },
+        None => {
+          //if the stub was invalidated, compile another one
+          compile_time += self.parse_stub(optimize, logging);
+        },
+      }
+    }
+  }
+  pub fn new(bios_filename: &String, infile: Option<&String>, gpu_logging: bool,
+             wx: u32, wy: u32) -> io::Result<Self> {
+    let console = Console::new(bios_filename, infile, gpu_logging, wx, wy)?;
+    Ok(Self {
+        console,
+        stubs: Default::default(),
+        ranges_compiled: Default::default(),
+    })
+  }
+  fn parse_stub(&mut self, optimize: bool, logging: bool) -> Duration {
+    let t0 = Instant::now();
+    let mut operations = Vec::new();
+    let start = self.console.r3000.pc();
+    let mut op = self.console.resolve_memresponse(self.console.memory.read_word(start));
+    let mut address = start;
+    let mut tagged = self.tag_insn(op, logging);
+    //add all instructions before the next jump to the stub
+    while tagged.is_some() {
+      if op != 0x00 {
+        operations.push((op, tagged.take().expect("")));
+      }
+      address = address.wrapping_add(4);
+      op = self.console.resolve_memresponse(self.console.memory.read_word(address));
+      //println!("{:#x}", op);
+      tagged = self.tag_insn(op, logging);
+    }
+    //do stub analysis and optimizations here
+    //let mut compiled_stub = if optimize {
+    //  self.compile_optimized_stub(&mut operations, logging)
+    //} else {
+    //  self.compile_stub(&mut operations, logging)
+    //};
+    let mut compiled_stub = self.compile_stub(&mut operations, logging);
+
+    //println!("jump {:#x} at {:#x}", op, address);
+    //get the jump instruction that ended the block
+    let jump_op = op;
+    let compiled_jump = self.compile_jump(op, logging);
+    compiled_stub.push(compiled_jump);
+
+    let mut len = operations.len() as u32 + 1;
+    //if the jump was not a SYSCALL
+    if jump_op != 0xc {
+      //add the branch delay slot to the stub
+      address = address.wrapping_add(4);
+      let op = self.console.resolve_memresponse(self.console.memory.read_word(address));
+      //println!("branch delay slot contained {:#x}", op);
+      let compiled = self.compile_opcode(op, logging);
+      //println!("{:#x} followed by {:#x}", jump_op, op);
+      compiled_stub.push(compiled.expect("Consecutive jumps are not allowed in the MIPS ISA"));
+      len += 1;
+    }
+    let stub = Stub {
+      operations: compiled_stub,
+      final_pc: Console::physical(address),
+      len,
+    };
+    self.stubs.insert(Console::physical(start), stub);
+    let end = Console::physical(address);
+    self.ranges_compiled.get_mut(&end)
+                        .map(|v| {
+                          v.push(Console::physical(start));
+                        })
+                        .or_else(|| {
+                          self.ranges_compiled.insert(end, vec![Console::physical(start)]);
+                          None
+                        });
+    let t1 = Instant::now();
+    t1 - t0
+  }
+  fn cache_invalidation(&mut self, address: Register) {
+    let deleted_stub = self.stubs.remove(&address).unwrap();
+    let overlapping_blocks = self.ranges_compiled.get(&deleted_stub.final_pc())
+                                                 .unwrap()
+                                                 .iter()
+                                                 .copied()
+                                                 .filter(|&start| address <= start)
+                                                 .collect::<Vec<Register>>();
+    overlapping_blocks.iter()
+                      .for_each(|s| {
+                        self.stubs.remove(&s).unwrap();
+                      });
+    self.ranges_compiled
+        .entry(deleted_stub.final_pc())
+        .and_modify(|v| {
+          v.retain(|&start| start < address);
+        });
+  }
+}
---
+++ src/x64_jit/optimize.rs
@@ -0,0 +1,285 @@
+use crate::register::Register;
+use crate::register::BitBang;
+use crate::r3000::MaybeSet;
+use crate::r3000::DelayedWrite;
+use crate::r3000::Name;
+use crate::x64_jit::X64JIT;
+use crate::console::Console;
+use super::insn_ir::Insn;
+use super::insn_ir::Kind;
+use crate::common::*;
+
+impl X64JIT {
+  pub(super) fn compile_stub(&mut self, operations: &mut Vec<(Register, Insn)>, logging: bool) -> Vec<Box<dyn Fn(&mut Console)>> {
+    let mut ret = Vec::new();
+    for (op, _) in operations {
+      ret.push(self.compile_opcode(*op, logging).expect(""));
+    };
+    ret
+  }
+  fn emit_optimized_read_byte(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let result = vm.resolve_memresponse(vm.memory.read_byte(constant_address));
+      vm.delayed_writes.push_back(DelayedWrite::new(Name::Rn(t), result));
+      if logging {
+        println!("R{} = [{:#x} + {:#x}] \n  = [{:#x}] \n  = {:#x} {}",
+                  t, constant, imm16, constant_address, result, stringify!(read_byte));
+      }
+    }));
+  }
+  fn emit_optimized_read_half(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let result = vm.resolve_memresponse(vm.memory.read_half(constant_address));
+      vm.delayed_writes.push_back(DelayedWrite::new(Name::Rn(t), result));
+      if logging {
+        println!("R{} = [{:#x} + {:#x}] \n  = [{:#x}] \n  = {:#x} {}",
+                  t, constant, imm16, constant_address, result, stringify!(read_byte));
+      }
+    }));
+  }
+  fn emit_optimized_read_word(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let result = vm.resolve_memresponse(vm.memory.read_word(constant_address));
+      vm.delayed_writes.push_back(DelayedWrite::new(Name::Rn(t), result));
+      if logging {
+        println!("R{} = [{:#x} + {:#x}] \n  = [{:#x}] \n  = {:#x} {}",
+                  t, constant, imm16, constant_address, result, stringify!(read_byte));
+      }
+    }));
+  }
+  fn emit_optimized_write_byte(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let rt = vm.r3000.nth_reg(t);
+      if logging {
+        println!("optimized [{:#x} + {:#x}] = [{:#x}] \n  = R{}\n  = {:#x} {}",
+                  constant, imm16, constant_address, t, rt, stringify!(write_byte));
+      }
+      if !vm.cop0.cache_isolated() {
+        vm.write_byte(constant_address, rt);
+      } else {
+        if logging {
+          println!("ignoring write while cache is isolated");
+        }
+      }
+    }));
+  }
+  fn emit_optimized_write_half(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let rt = vm.r3000.nth_reg(t);
+      if logging {
+        println!("optimized [{:#x} + {:#x}] = [{:#x}] \n  = R{}\n  = {:#x} {}",
+                  constant, imm16, constant_address, t, rt, stringify!(write_half));
+      }
+      if !vm.cop0.cache_isolated() {
+        vm.write_half(constant_address, rt);
+      } else {
+        if logging {
+          println!("ignoring write while cache is isolated");
+        }
+      }
+    }));
+  }
+  fn emit_optimized_write_word(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, op: Register, constant: Register, logging: bool) {
+    let t = get_rt(op);
+    let imm16 = get_imm16(op).half_sign_extended();
+    let constant_address = constant.wrapping_add(imm16);
+    ret.push(Box::new(move |vm| {
+      let rt = vm.r3000.nth_reg(t);
+      if logging {
+        println!("optimized [{:#x} + {:#x}] = [{:#x}] \n  = R{}\n  = {:#x} {}",
+                  constant, imm16, constant_address, t, rt, stringify!(write_word));
+      }
+      if !vm.cop0.cache_isolated() {
+        vm.write_word(constant_address, rt);
+      } else {
+        if logging {
+          println!("ignoring write while cache is isolated");
+        }
+      }
+    }));
+  }
+  fn emit_store_constant(ret: &mut Vec<Box<dyn Fn(&mut Console)>>, result: Register, name: Register, logging: bool) {
+    ret.push(Box::new(move |vm| {
+      let out = vm.r3000.nth_reg_mut(name);
+      vm.modified_register = out.maybe_set(result);
+      if logging {
+        println!("> optimized register load R{} = {:#x}", name, result);
+      }
+    }));
+  }
+  pub(super) fn compile_optimized_stub(&mut self, operations: &mut Vec<(Register, Insn)>, logging: bool) -> Vec<Box<dyn Fn(&mut Console)>> {
+    let initial_n = operations.len();
+    let mut ret = Vec::new();
+    let mut const_registers: [Option<Register>; 32] = [None; 32];
+    for (i, (op, tag)) in operations.iter().enumerate() {
+      const_registers[0] = Some(0);
+      let opcode = get_primary_field(*op);
+      match opcode {
+        0x00 => {
+          let second_opcode = get_secondary_field(*op);
+          match second_opcode {
+            0x21 | 0x23..=0x27 | 0x2A | 0x2B => {
+              match (const_registers[tag.input_i(0)], const_registers[tag.input_i(1)]) {
+                (Some(c1), Some(c2)) => {
+                  let result = match second_opcode {
+                    0x21 => c1.wrapping_add(c2),
+                    0x23 => c1.wrapping_sub(c2),
+                    0x24 => c1.and(c2),
+                    0x25 => c1.or(c2),
+                    0x26 => c1.xor(c2),
+                    0x27 => c1.nor(c2),
+                    0x2A => c1.signed_compare(c2),
+                    0x2B => c1.compare(c2),
+                    _ => unreachable!("{:x}", get_secondary_field(*op)),
+                  };
+                  let output = tag.output().expect("");
+                  X64JIT::emit_store_constant(&mut ret, result, output, logging);
+                  const_registers[output as usize] = Some(result);
+                },
+                _ => {
+                  let output = tag.output().expect("");
+                  const_registers[output as usize] = None;
+                  ret.push(self.compile_opcode(*op, logging).expect(""));
+                },
+              }
+            },
+            _ => {
+              match tag.output() {
+                Some(output) => const_registers[output as usize] = None,
+                None => {},
+              }
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x09..=0x0E => {
+          match const_registers[tag.input_i(0)] {
+            Some(constant) => {
+              let result = match opcode {
+                0x09 => constant.wrapping_add(get_imm16(*op).half_sign_extended()),
+                0x0A => constant.signed_compare(get_imm16(*op)),
+                0x0B => constant.compare(get_imm16(*op)),
+                0x0C => constant.and(get_imm16(*op)),
+                0x0D => constant.or(get_imm16(*op)),
+                0x0E => constant.xor(get_imm16(*op)),
+                _ => unreachable!(""),
+              };
+              let output = tag.output().expect("");
+              //if previous operation was a LUI with the same output
+              if i != 0 {
+                if get_primary_field(operations[i - 1].0) == 0x0F &&
+                  operations[i - 1].1.output() == Some(output) {
+                  ret.pop();
+                }
+              }
+              X64JIT::emit_store_constant(&mut ret, result, output, logging);
+              //mark output as constant
+              const_registers[output as usize] = Some(result);
+            },
+            None => {
+              let output = tag.output().expect("");
+              const_registers[output as usize] = None;
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x0F => {
+          //LUI
+          let result = get_imm16(*op) << 16;
+          let output = tag.output().expect("");
+          const_registers[output as usize] = Some(result);
+          X64JIT::emit_store_constant(&mut ret, result, output, logging);
+        },
+        0x23 => {
+          let output = tag.output().expect("");
+          const_registers[output as usize] = None;
+          match const_registers[tag.input_i(0)] {
+            Some(addr) => {
+              X64JIT::emit_optimized_read_word(&mut ret, *op, addr, logging);
+            },
+            None => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x24 => {
+          let output = tag.output().expect("");
+          const_registers[output as usize] = None;
+          match const_registers[tag.input_i(0)] {
+            Some(addr) => {
+              X64JIT::emit_optimized_read_byte(&mut ret, *op, addr, logging);
+            },
+            None => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x25 => {
+          let output = tag.output().expect("");
+          const_registers[output as usize] = None;
+          match const_registers[tag.input_i(0)] {
+            Some(addr) => {
+              X64JIT::emit_optimized_read_half(&mut ret, *op, addr, logging);
+            },
+            None => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x28 => {
+          match (const_registers[tag.input_i(0)], const_registers[tag.input_i(1)]) {
+            (Some(addr), _) => {
+              X64JIT::emit_optimized_write_byte(&mut ret, *op, addr, logging);
+            },
+            _ => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x29 => {
+          match (const_registers[tag.input_i(0)], const_registers[tag.input_i(1)]) {
+            (Some(addr), _) => {
+              X64JIT::emit_optimized_write_half(&mut ret, *op, addr, logging);
+            },
+            _ => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        0x2B => {
+          match (const_registers[tag.input_i(0)], const_registers[tag.input_i(1)]) {
+            (Some(addr), _) => {
+              X64JIT::emit_optimized_write_word(&mut ret, *op, addr, logging);
+            },
+            _ => {
+              ret.push(self.compile_opcode(*op, logging).expect(""));
+            },
+          }
+        },
+        _ => {
+          match tag.output() {
+            Some(output) => const_registers[output as usize] = None,
+            None => {},
+          }
+          ret.push(self.compile_opcode(*op, logging).expect(""));
+        },
+      }
+    }
+    ret
+  }
+}
