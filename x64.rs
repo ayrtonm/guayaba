d82a0e2de9346ddedeff11d190ac4dea57197dfe
Ayrton <a.munoz3327@gmail.com>
2020-05-17 02:46:48 -0400

    started working on the x64 JIT

--- x64.rs
+++ x64.rs
@@ -2,6 +2,21 @@
 use std::io;
 use memmap::{Mmap, MmapMut};
 
+struct Toy {
+  f: fn(u32) -> u32,
+}
+
+impl Toy {
+  pub fn new(f: fn(u32) -> u32) -> Self {
+    Toy {
+      f
+    }
+  }
+  pub fn run(&self, x: u32) -> u32 {
+    (self.f)(x)
+  }
+}
+
 fn main() -> io::Result<()> {
   let mut buffer = Vec::new();
   buffer.push(0x48);
@@ -14,8 +29,9 @@ fn main() -> io::Result<()> {
   let addr = mmap.as_ptr();
   unsafe {
     let f = std::mem::transmute::<*const u8, fn(u32) -> u32>(addr);
+    let toy = Toy::new(f);
     for i in 0..=10 {
-      println!("{}", f(i));
+      println!("{}", toy.run(i));
     }
   };
   Ok(())
