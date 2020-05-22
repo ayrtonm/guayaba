6e9b775c56c05b6c207c088e33e833a213a4cbe8
Ayrton <a.munoz3327@gmail.com>
2020-05-22 11:52:33 -0400

    moved default optimized stub to a macro

--- src/caching_interpreter/optimized_stubs.rs
+++ src/caching_interpreter/optimized_stubs.rs
@@ -7,10 +7,36 @@ impl Block {
   pub fn create_optimized_stubs(tagged_opcodes: &Vec<Insn>, logging: bool) -> Vec<Stub> {
     let mut ret = Vec::new();
     let mut constant_table = [None; 32];
+    macro_rules! default_stub {
+      ($insn:expr) => {
+        {
+          $insn.output().map(|output| constant_table[output] = None);
+          match $insn.output() {
+            Some(output) => {
+              if output == 0 {
+                ret.push(Stub::from_closure(Box::new(move |vm| None)));
+              } else {
+                ret.push(Stub::new(&$insn, logging));
+              }
+            },
+            None => {
+              ret.push(Stub::new(&$insn, logging));
+            },
+          }
+        }
+      };
+    }
     for insn in tagged_opcodes {
       constant_table[0] = Some(0);
       let op = insn.op();
       match get_primary_field(op) {
+        0x00 => {
+          match get_secondary_field(op) {
+            _ => {
+              default_stub!(insn);
+            },
+          }
+        },
         0x0F => {
           //LUI
           let output = insn.output().expect("LUI should have an output");
@@ -18,19 +44,7 @@ impl Block {
           ret.push(Stub::new(&insn, logging));
         },
         _ => {
-          insn.output().map(|output| constant_table[output] = None);
-          match insn.output() {
-            Some(output) => {
-              if output == 0 {
-                ret.push(Stub::from_closure(Box::new(move |vm| None)));
-              } else {
-                ret.push(Stub::new(&insn, logging));
-              }
-            },
-            None => {
-              ret.push(Stub::new(&insn, logging));
-            },
-          }
+          default_stub!(insn);
         },
       }
     };
