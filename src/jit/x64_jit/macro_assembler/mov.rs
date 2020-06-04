442ca61625f9d8030dbedd19ca0699a568012dfa
Ayrton <a.munoz3327@gmail.com>
2020-06-04 16:06:48 -0400

    implemented and tested movl_rm_offset

--- src/jit/x64_jit/macro_assembler/mov.rs
+++ src/jit/x64_jit/macro_assembler/mov.rs
@@ -53,59 +53,29 @@ impl MacroAssembler {
       };
     }
   }
-  //FIXME: doesn't seem like this'll work with extended registers i.e. r8-r15
-  //fn specify_rm(src: u32, ptr_dest: u32) -> u8 {
-  //  (src.lowest_bits(3) << 3) as u8 | (ptr_dest.lowest_bits(3) as u8)
-  //}
-  //fn specify_mr(src: u32, ptr_dest: u32) -> u8 {
-  //  MacroAssembler::specify_rm(src, ptr_dest)
-  //}
-  //fn specify_sib(base: u32, index: u32, scale: u32) -> u8 {
-  //  let scale = match scale {
-  //    1 => 0,
-  //    2 => 1,
-  //    4 => 2,
-  //    8 => 3,
-  //    _ => unreachable!("invalid SIB scale"),
-  //  } << 6;
-  //  base.lowest_bits(3) as u8 |  ((index.lowest_bits(3) as u8) << 3) | scale
-  //}
-  //pub fn emit_movl_rm(&mut self, src: u32, ptr_dest: u32) {
-  //  if src.nth_bit_bool(3) {
-  //    self.buffer.push(MacroAssembler::REXRB);
-  //  } else {
-  //    self.buffer.push(MacroAssembler::REXB);
-  //  }
-  //  let specify_regs = MacroAssembler::specify_rm(src, ptr_dest); 
-  //  self.buffer.push(0x89);
-  //  self.buffer.push(specify_regs);
-  //}
-  //pub fn emit_movl_rm_sib_offset(&mut self, src: u32, ptr_dest: u32, idx: u32, scale: u32, offset: u32) {
-  //  let specify_regs = MacroAssembler::specify_rm(src, ptr_dest);
-  //  let sib = MacroAssembler::specify_sib(ptr_dest, idx, scale);
-  //  self.buffer.push(0x89);
-  //  self.buffer.push(sib);
-  //  self.buffer.push(specify_regs);
-  //  self.emit_imm32(offset);
-  //}
-  //pub fn emit_movl_rm_offset(&mut self, src: u32, ptr_dest: u32, offset: u32) {
-  //  let specify_regs = MacroAssembler::specify_rm(src, ptr_dest); 
-  //  //let sib = MacroAssembler::specify_sib(ptr_dest, ptr_dest, 1);
-  //  self.buffer.push(0x89);
-  //  //self.buffer.push(sib);
-  //  self.buffer.push(specify_regs);
-  //  self.emit_imm32(offset);
-  //}
-  //pub fn emit_movl_mr(&mut self, ptr_src: u32, dest: u32) {
-  //  if ptr_src.nth_bit_bool(3) {
-  //    self.buffer.push(MacroAssembler::REXRB);
-  //  } else {
-  //    self.buffer.push(MacroAssembler::REXB);
-  //  }
-  //  let specify_regs = MacroAssembler::specify_mr(ptr_src, dest);
-  //  self.buffer.push(0x8b);
-  //  self.buffer.push(specify_regs);
-  //}
+  pub fn emit_movl_rm_offset(&mut self, src: u32, ptr: u32, offset: i32) {
+    if offset == 0 {
+      self.emit_movl_rm(src, ptr);
+    } else {
+      self.emit_conditional_rexrb(src, ptr);
+      self.buffer.push(0x89);
+      if ptr.lowest_bits(3) == 5 {
+        self.buffer.push(0x45 | (src.lowest_bits(3) << 3) as u8);
+      } else {
+        self.buffer.push(0x40 | (src.lowest_bits(3) << 3) as u8 | ptr.lowest_bits(3) as u8);
+        if ptr.lowest_bits(3) == 4 {
+          self.buffer.push(0x24);
+        };
+      }
+      match offset {
+        0 => unreachable!(""),
+        -128..=127 => self.buffer.push(offset as u8),
+        _ => self.emit_imm32(offset as u32),
+      }
+    }
+  }
+  pub fn emit_movl_mr_offset(&mut self, ptr: u32, dest: u32, offset: i32) {
+  }
 }
 
 #[cfg(test)]
@@ -196,8 +166,6 @@ mod tests {
   fn movl_rm() {
     for ptr in MacroAssembler::all_regs() {
       for src in MacroAssembler::test_regs() {
-        let memory: u32 = 0xfeaf_fb24;
-        let memory_location = &memory as *const u32 as u64;
         let mut masm = MacroAssembler::new();
         masm.emit_movl_ir(0x5324_bcda, src);
         masm.emit_push_r32(1);
@@ -215,6 +183,34 @@ mod tests {
         //but we might as well run them anyway to make sure they don't segfault
         if ptr != src {
           assert_eq!(out, 0x5324_bcda);
+        }
+      }
+    }
+  }
+
+  #[test]
+  fn movl_rm_offset() {
+    for ptr in MacroAssembler::all_regs() {
+      for src in MacroAssembler::test_regs() {
+        let mut masm = MacroAssembler::new();
+        masm.emit_movl_ir(0xbdef_2398, src);
+        masm.emit_push_r32(1);
+        masm.emit_push_r32(1);
+        masm.emit_push_r32(1);
+        masm.emit_movq_rr(X64RegNum::RSP as u32, ptr);
+        masm.emit_movl_rm_offset(src, ptr, 16);
+        masm.emit_pop_r32(1);
+        masm.emit_pop_r32(1);
+        masm.emit_pop_r32(0);
+        let jit_fn = masm.compile_buffer().unwrap();
+        let out: u32;
+        unsafe {
+          asm!("callq *%rbp"
+              :"={rax}"(out)
+              :"{rbp}"(jit_fn.name));
+        }
+        if ptr != src {
+          assert_eq!(out, 0xbdef_2398);
         }
       }
     }
